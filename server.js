7e64ae8608c35e603828954d0ea0ff58d69cc359
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-03-30 21:22:53 +0900

    Support routing

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const Note = require('./models/note');
+
+const notesRoutes = require('./routes/notes');
 
 const app = express();
-
-var notes = [];
 
 /* Connect to database */
 mongoose.connect("mongodb+srv://manager:XDeU8k0xSsInSY3f@clusterfree-g7tel.mongodb.net/notes-app?retryWrites=true&w=majority")
@@ -41,74 +40,6 @@ app.use((req, res, next) => {
   next(); // Trigger other middlewares
 });
 
-app.post("/notes", (req, res) => {
-  console.log("Received post request...");
-
-  /*
-   * Method 1: mapping
-  const note = new Note({
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    category: req.body.category,
-  });
-   *
-   * Method 2: direct
-   */
-  const note = new Note( req.body );
-
-  notes.push(note);
-  note.save().then(result => {
-    res.status(201).send(result);
-  });
-});
-
-app.get("/notes", (req, res) => {
-  console.log("Received get request...");
-
-  Note.find()
-    .then(documents => {
-      res.status(200).send(documents);
-    });
-});
-
-app.get("/notes/:id", (req, res) => {
-  console.log("Received get request...");
-
-  Note.findOne({_id: req.params.id})
-    .then(note => {
-      if (note) {
-        res.status(200).send(note);
-      } else {
-        res.status(404).json({message: 'Note not found!'});
-      }
-    });
-});
-
-app.put("/notes/:id", (req, res) => {
-  console.log("Received put request for " + req.params.id);
-
-  const note = new Note({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    category: req.body.category,
-    author: req.body.author,
-    });
-
-  Note.updateOne({_id: req.params.id}, note)
-    .then(documents => {
-      res.status(200).send(documents);
-    });
-});
-
-app.delete("/notes/:id", (req, res) => {
-  console.log('Received delete request for ' + req.params.id);
-
-  Note.deleteOne({_id: req.params.id})
-    .then(result => {
-      res.status(200).end();
-    });
-});
+app.use("/notes", notesRoutes);
 
 app.listen(3000);
