115d2bc3f65ab02f9bb6dfa348b1205d11c64114
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-04-11 00:21:49 +0900

    New Feature: Drag and sort notes

--- models/note.js
+++ models/note.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const noteSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  personal: { type: Boolean, default: false },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User001", required: true }
+  personal: { type: Boolean },
+  category: { type: [String] },
+  created: { type: Date, required: true },
+  updated: { type: Date, required: true },
+  rank: { type: Number, required: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User001", required: true },
 });
 
-module.exports = mongoose.model('Note001', noteSchema);
+module.exports = mongoose.model('Note004', noteSchema);
