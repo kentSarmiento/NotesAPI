f538b734f3e75487739b5c7de7bf908f16961486
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-04-30 15:15:47 +0900

    Allow unauthorized view of data

--- middleware/get-auth.js
+++ middleware/get-auth.js
@@ -2,16 +2,19 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) =>  {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
-    req.authInfo = {
-      username: decodedToken.username,
-      userId: decodedToken.userId
-    };
-    if (req.query.userId && req.authInfo.userId !== req.query.userId) {
-      throw new Error("Invalid authorization token used");
+    if (!req.headers.authorization) next(); // Allow unauthorized view of data
+    else {
+      const token = req.headers.authorization.split(" ")[1];
+      const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+      req.authInfo = {
+        username: decodedToken.username,
+        userId: decodedToken.userId
+      };
+      if (req.query.userId && req.authInfo.userId !== req.query.userId) {
+        throw new Error("Invalid authorization token used");
+      }
+      next();
     }
-    next();
   } catch (error) {
     if (req.query.userId) {
       res.status(401).json({message: error.message});
